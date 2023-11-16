910e0af2d6a1112b2ce032fc6271a3b8558520c5
idzark <a.idzikowski91@gmail.com>
2023-11-16 10:07:48 +0100

    Add Angular config (#51)

---
+++ packages/angular/projects/data-den-angular/src/lib/data-den-angular.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { DataDenAngularComponent } from './data-den-angular.component';
+
+@NgModule({
+  declarations: [DataDenAngularComponent],
+  imports: [],
+  exports: [DataDenAngularComponent],
+})
+export class DataDenAngularModule {}
