af39477cba2a9df3d37b8cf02d6463be78d85046
Lukasz Michalik <l.michalik004@gmail.com>
2023-11-17 12:21:32 +0100

    Information about the row status

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -110,7 +110,7 @@ const options: DataDenOptions = {
   paginationOptions: {
     pageSize: 10,
     pageSizeOptions: [5, 10, 12, 20],
-    ofText: 'z',
+    ofText: 'of',
   },
   quickFilter: true,
   quickFilterOptions: {
