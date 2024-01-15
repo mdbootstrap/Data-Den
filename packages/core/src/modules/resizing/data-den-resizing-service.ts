7d01996f3851b926b2bf46244a15bace693aaeb6
Lukasz Michalik <l.michalik004@gmail.com>
2024-01-15 15:59:59 +0100

    fix resizing

--- packages/core/src/modules/resizing/data-den-resizing-service.ts
+++ packages/core/src/modules/resizing/data-den-resizing-service.ts
@@ -117,8 +117,13 @@ export class DataDenResizingService {
     }
 
     const movementX = this.#isResizingPinnedRightColumn ? -event.movementX : event.movementX;
+    const currentWidth = this.#currentHeader?.style.width;
+    const headerLeft = this.#currentHeader.querySelector(`.${this.#options.cssPrefix}header-resizer`);
+    const newWidth = parseInt(currentWidth || '0') + movementX;
 
-    this.#resizeCurrentColumn(movementX);
+    if ((movementX > 0 && event.clientX < headerLeft.getClientRects()[0].left) || newWidth < 45) return;
+
+    this.#resizeCurrentColumn(newWidth);
     this.#updateRemainingColumnsPosition(movementX);
     this.PubSub.publish('info:resizing:start', {
       currentColIndex: this.#headers.indexOf(this.#currentHeader),
@@ -127,10 +132,7 @@ export class DataDenResizingService {
     });
   }
 
-  #resizeCurrentColumn(movementX: number) {
-    const currentWidth = this.#currentHeader?.style.width;
-    const newWidth = parseInt(currentWidth || '0') + movementX;
-
+  #resizeCurrentColumn(newWidth: number) {
     this.#currentCol.forEach((cell) => (cell.style.width = `${newWidth}px`));
   }
 
