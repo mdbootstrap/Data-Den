b123ffcd60d3189a2695f70b0c2fa7ab7597fb41
Pan Seba <skaczmarek03@gmail.com>
2024-01-30 20:57:21 +0100

    Fix logout & DPL links

--- packages/docs/static/js/dynamic-parts-loader/components/navbar-right-buttons.js
+++ packages/docs/static/js/dynamic-parts-loader/components/navbar-right-buttons.js
@@ -11,7 +11,8 @@ export const navbarNewRightButtons = DPL.defineComponent({
   template: (userData) => {
     const cartIcon = `<a
             class="mr-4 text-neutral-600 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
-            href="#">
+            href="https://mdbootstrap.com/cart/"
+            target="_blank">
             <span class="[&>svg]:w-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -25,13 +26,14 @@ export const navbarNewRightButtons = DPL.defineComponent({
               <span class="absolute -mt-4 ml-2.5 rounded-full bg-danger px-[0.35em] py-[0.15em] text-[0.6rem] font-bold leading-none text-white">${userData.cartContentsCount}</span>
           </a>`;
 
-    const navbarNotifications = (userData.notifications || [])
+    const userNotifications = userData.notifications || [];
+    const navbarNotifications = userNotifications
       .map(
         (n) => `<li>
   <a
     class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
     href="${n.link}"
-    ${n.link.indexOf('getdataden.com') ? 'target="_blank" rel="nofollow"' : ''}
+    ${n.link.indexOf('getdataden.com') === -1 ? 'target="_blank" rel="nofollow"' : ''}
     data-notification-date="${n.date}"
     data-te-dropdown-item-ref
   >${n.value}</a
@@ -52,21 +54,24 @@ export const navbarNewRightButtons = DPL.defineComponent({
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="/profile/settings"
+      href="https://mdbootstrap.com/profile/settings"
+      target="_blank"
       data-te-dropdown-item-ref
     >Account Settings</a>
   </li>
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="/profile/my-orders"
+      href="https://mdbootstrap.com/profile/my-orders"
+      target="_blank"
       data-te-dropdown-item-ref
     >My Orders</a>
   </li>
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="/support/?user=${userData.id}"
+      href="https://mdbootstrap.com/support/?user=${userData.id}"
+      target="_blank"
       data-te-dropdown-item-ref
     >My Questions</a>
   </li>
@@ -74,6 +79,7 @@ export const navbarNewRightButtons = DPL.defineComponent({
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
       href="https://dashboard.mdbgo.com/#/projects"
+      target="_blank"
       data-te-dropdown-item-ref
     >Projects</a>
   </li>
@@ -81,21 +87,24 @@ export const navbarNewRightButtons = DPL.defineComponent({
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="/snippets?user=${userData.id}&status=1&top=all"
+      href="https://mdbootstrap.com/snippets?user=${userData.id}&status=1&top=all"
+      target="_blank"
       data-te-dropdown-item-ref
     >Public snippets</a>
   </li>
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="/snippets?user=${userData.id}&status=4&top=all"
+      href="https://mdbootstrap.com/snippets?user=${userData.id}&status=4&top=all"
+      target="_blank"
       data-te-dropdown-item-ref
     >Private snippets</a>
   </li>
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="/snippets?user=${userData.id}&status=2&top=all"
+      href="https://mdbootstrap.com/snippets?user=${userData.id}&status=2&top=all"
+      target="_blank"
       data-te-dropdown-item-ref
     >Draft snippets</a>
   </li>
@@ -104,7 +113,8 @@ export const navbarNewRightButtons = DPL.defineComponent({
             ? `<li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="/snippets?status=4&top=all"
+      href="https://mdbootstrap.com/snippets?status=4&top=all"
+      target="_blank"
       data-te-dropdown-item-ref
     >All private snippets</a>
   </li>`
@@ -114,7 +124,8 @@ export const navbarNewRightButtons = DPL.defineComponent({
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="${location.origin}/wp-login.php?action=logout&redirect_to=${encodeURIComponent(location.href)}"
+      href="#"
+      data-logout-button-ref
       data-te-dropdown-item-ref
     >Log Out</a>
   </li>
@@ -123,9 +134,35 @@ export const navbarNewRightButtons = DPL.defineComponent({
     return `<div class="relative flex items-center">
 ${userData.cartContentsCount > 0 ? cartIcon : ''}
 
+<div class="relative" data-te-dropdown-ref data-te-dropdown-alignment="end">
 ${
-  navbarNotifications.length > 0
-    ? `<ul
+  userNotifications.length > 0
+    ? `<a class="hidden-arrow mr-4 flex items-center text-neutral-600 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+  href="#"
+  id="dropdownMenuButton1"
+  role="button"
+  data-te-dropdown-toggle-ref
+  aria-expanded="false">
+  <!-- Dropdown trigger icon -->
+  <span class="[&>svg]:w-5">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="h-5 w-5">
+      <path
+        fill-rule="evenodd"
+        d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
+        clip-rule="evenodd" />
+    </svg>
+  </span>
+  <!-- Notification counter -->
+  <span
+    class="absolute -mt-4 ml-2.5 rounded-full bg-danger px-[0.35em] py-[0.15em] text-[0.6rem] font-bold leading-none text-white"
+    >${userNotifications.length}</span
+  >
+</a>
+<ul
               class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
               aria-labelledby="dropdownMenuButton1"
               data-te-dropdown-menu-ref>
@@ -134,6 +171,7 @@ ${
 `
     : ``
 }
+</div>
 </div>
 
 <div class="relative" data-te-dropdown-ref data-te-dropdown-alignment="end">
