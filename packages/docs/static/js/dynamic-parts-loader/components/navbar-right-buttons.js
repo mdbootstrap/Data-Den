36c368487497d1ad05c9091a3ebe500196e5d6e0
Pan Seba <skaczmarek03@gmail.com>
2024-03-12 13:13:00 +0100

    Integrate checkout (#105)

--- packages/docs/static/js/dynamic-parts-loader/components/navbar-right-buttons.js
+++ packages/docs/static/js/dynamic-parts-loader/components/navbar-right-buttons.js
@@ -62,7 +62,7 @@ export const navbarNewRightButtons = DPL.defineComponent({
   <li>
     <a
       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30"
-      href="https://mdbootstrap.com/profile/my-orders"
+      href="/billing"
       target="_blank"
       data-te-dropdown-item-ref
     >My Orders</a>
