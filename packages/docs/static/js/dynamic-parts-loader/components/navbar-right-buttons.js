65427feaf886ddf8dc0211f509009d04bf305889
Piotr Gusciora <p.gusciora@mdbootstrap.com>
2024-03-15 15:38:31 +0100

    improved navbar button

--- packages/docs/static/js/dynamic-parts-loader/components/navbar-right-buttons.js
+++ packages/docs/static/js/dynamic-parts-loader/components/navbar-right-buttons.js
@@ -153,6 +153,14 @@ ${
     : ``
 }
 </div>
+<!-- new -->
+<a
+  href="/general/pro/"
+  type="button"
+  class="ms-2 inline-block rounded bg-primary px-4 pb-[6px] pt-[7px] text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+  Get early Access
+</a>
+<!-- new -->
 </div>
 
 <div class="relative" data-te-dropdown-ref data-te-dropdown-alignment="end">
